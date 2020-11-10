dcdda5bee66f5143d1742c13c67e40382b2b5baa
Ariye Bar-tov <ariyeb@gmail.com>
2020-11-10 18:58:54 +0200

    adding login form

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginForm = () => {
+	const [email, setEmail] = useState("");
+	// const emailState = useState("");
+	const [password, setPassword] = useState("");
+	const [isEmailinputValid, setIsEmailInputValid] = useState(true);
+	const [isPasswordInputValid, setIsPasswordInputValid] = useState(true);
+
+	const isFormInavlid = () => {
+		return email === "" || password === "";
+	};
+
+	const onBlurEmailInput = (event) => {
+		const theEmail = event.target.value.trim();
+		if (theEmail === "") {
+			setEmail("");
+			// emailState[1]("")
+			setIsEmailInputValid(false);
+		} else {
+			setEmail(theEmail);
+			setIsEmailInputValid(true);
+		}
+	};
+
+	const onBlurPasswordInput = (event) => {
+		const thePassword = event.target.value.trim();
+		setPassword(thePassword === "" ? "" : thePassword);
+		setIsPasswordInputValid(thePassword !== "");
+	};
+
+	const onSubmitform = (event) => {
+		event.preventDefault();
+		console.log("login form:", email, password);
+	};
+
 	return (
 		<div className="login-form">
 			<h3>Login</h3>
-			<form>
-				<input placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<button type="submit">Submit</button>
+			<form onSubmit={onSubmitform}>
+				<input placeholder="Email" onBlur={onBlurEmailInput} />
+				{!isEmailinputValid && <div className="invalid-message">You must enter your email.</div>}
+				<input type="password" placeholder="Password" onBlur={onBlurPasswordInput} />
+				{!isPasswordInputValid && <div className="invalid-message">You must enter your password.</div>}
+				<button type="submit" disabled={isFormInavlid()}>Submit</button>
 			</form>
 		</div>
 	);
