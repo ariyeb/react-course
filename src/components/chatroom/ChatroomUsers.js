46e56079bea4927211a504da23a458da27529c30
Ariye Bar-tov <ariyeb@gmail.com>
2020-11-11 19:58:57 +0200

    adding serach users

--- src/components/chatroom/ChatroomUsers.js
+++ src/components/chatroom/ChatroomUsers.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SearchUsers from './SearchUsers';
 
 const ChatroomUsers = (props) => {
+    const [usersToDisplay, setUsersToDisplay] = useState([...props.users]);
+
+    const searchUsers = (searchValue) => {
+        const users = [...props.users];
+        setUsersToDisplay(searchValue === "" ?
+            users :
+            users.filter((user) => user.username.toLowerCase().includes(searchValue)));
+    };
 
     return (
         <div className="chatroom__users">
             <h3>Users</h3>
-            {props.users.map((user) => (
+            <SearchUsers searchUsers={searchUsers} />
+            {usersToDisplay.map((user) => (
                 <div className="user" key={user.id}>{user.username}</div>
             ))}
         </div>
