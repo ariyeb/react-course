9a973c3cbc56f6042bdb61c23bc6da5e6b9c0dbb
Ariye Bar-tov <ariyeb@gmail.com>
2020-11-11 21:02:47 +0200

    adding private message

--- src/components/chatroom/ChatroomUsers.js
+++ src/components/chatroom/ChatroomUsers.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import PrivateMessage from './PrivateMessage';
 import SearchUsers from './SearchUsers';
 
 const ChatroomUsers = (props) => {
     const [usersToDisplay, setUsersToDisplay] = useState([...props.users]);
+    const [privateMessageUser, setPrivateMessageUser] = useState(null);
 
     const searchUsers = (searchValue) => {
         const users = [...props.users];
@@ -11,13 +13,32 @@ const ChatroomUsers = (props) => {
             users.filter((user) => user.username.toLowerCase().includes(searchValue)));
     };
 
+    const sendPrivateMessage = () => {
+        setPrivateMessageUser(null);
+    };
+
     return (
         <div className="chatroom__users">
             <h3>Users</h3>
             <SearchUsers searchUsers={searchUsers} />
             {usersToDisplay.map((user) => (
-                <div className="user" key={user.id}>{user.username}</div>
+                <div
+                    className="user"
+                    key={user.id}
+                    onClick={() => {
+                        setPrivateMessageUser(user);
+                    }}
+                >
+                    {user.username}
+                </div>
             ))}
+            {
+                !!privateMessageUser &&
+                <PrivateMessage
+                    user={privateMessageUser}
+                    sendPrivateMessage={sendPrivateMessage}
+                />
+            }
         </div>
     );
 };
