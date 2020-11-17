4e3f8f7720c69eb1ea95b0a745abed295bd715ae
Ariye Bar-tov <ariyeb@gmail.com>
2020-11-17 18:39:23 +0200

    routing example

--- src/react-tutorial-playground/rouer-example/Aritcle.js
+++ src/react-tutorial-playground/rouer-example/Aritcle.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Article = (props) => {
-    console.log(props);
+    const history = useHistory();
+
+    const onClickButton = () => {
+        // props.history.push('/articles');
+        history.push("/articles");
+    };
     return (
         <div>
-            <h1>Article</h1>
+            <h1>Article: { props.match.params.name }</h1>
+            <button onClick={ onClickButton }>To Articles</button>
         </div>
     );
 };
